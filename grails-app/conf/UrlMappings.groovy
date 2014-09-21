541827a5fd1161a36e5fcc3b9e2e72d51e0ca350
Marcin Piczkowski <piczmar@wp.pl>
2014-09-22 00:38:20 +0200

    versioning in Accept-Version header

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -6,14 +6,18 @@ class UrlMappings {
                 // apply constraints here
             }
         }
-		"/pictures/lucky" (controller: "picture", action: "lucky", method: "GET")
+		"/pictures/lucky" (controller: "picture", action: "lucky", method: "GET") // this mapping must be placed before others because it overrides
+																				  // /pictures/:id endpoint
 		"/pictures"(resources: "picture"){
 			"/comments"(resources: "comment")
 		}
 		"/comments"(resources: "comment")
 		
-		"/v1/stats"(namespace: "v1", controller: "stats")
-		"/v2/stats"(namespace: "v2", controller: "stats")
+//		"/v1/stats"(namespace: "v1", controller: "stats")
+//		"/v2/stats"(namespace: "v2", controller: "stats")
+		
+		"/stats"(namespace: "v1", controller: "stats", version: '1.0')
+		"/stats"(namespace: "v2", controller: "stats", version: '2.0')
 		
         "/"(view:"/index")
         "500"(view:'/error')
